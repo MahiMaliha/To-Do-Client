c142033d694b47e32dcd58cd2401cd0522423761
MahiMaliha <jannatul15-9342@diu.edu.bd>
2022-05-19 05:36:37 +0600

    added

---
+++ src/components/home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AddProduct from '../addproducts/AddProduct';
+import ManageInventory from '../manageinventory/ManageInventory';
+const Home = () => {
+    
+    return (
+        <div>
+    
+          <ManageInventory></ManageInventory>
+        
+        </div>
+    );
+};
+
+export default Home;
